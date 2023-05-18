3056f067f2ac54c34fa71e929203c3d6c52584b1
Solairani <132639210+Solairani12@users.noreply.github.com>
2023-05-18 18:30:17 +0530

    Add files via upload

---
+++ server/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO `user` 
+(`id`, `first_name`,  `last_name`,    `email`,                 `education`,         `comments`,    `status`) VALUES
+(NULL, 'solai',      'rani',        'solai21gmail.com',         'b.voc',              '',          'active'),
+(NULL, 'ammu',       'kalai',       'ammukalai31gmail.com',     'b.sc',               '',          'active'),
+(NULL, 'myna',       'nandhini',    'myna23gmail.com',          'm.voc',              '',          'active'),
+(NULL, 'priya',      'dharshni',    'priya25gmail.com',         'm.sc',               '',          'active'),
+(NULL, 'ramya',      'panidiyan',   'ramya34gmail.com',         'b.voc',              '',          'active'),
+(NULL, 'hema',       'latha',       'hema56gmail.com',          'MBA',                '',          'active'),
+(NULL, 'aishwarya',  'lekshmi',    'asishwarya89gmail.com',     'b.sc',               '',          'active')
